aaac63ad05f09fbf51c5f13d44a35ba8b02f0e0a
William Rainey <williamhrainey@gmail.com>
2022-04-25 17:00:50 -0700

    played around a bit and started working on the home page

---
+++ mogged-supps/src/home.js
@@ -0,0 +1,17 @@
+import './home.css';
+import ScriptTag from 'react-script-tag';
+
+function Home() {
+    return (
+        <div>
+            <nav class='nav'>
+
+            </nav>
+            <div>
+
+            </div>
+        </div> 
+    );
+}
+
+export default Home;
