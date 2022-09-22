adebcfdecd2f30f8f68c9e03653652c167cfe69c
William Rainey <williamhrainey@gmail.com>
2022-09-22 14:57:12 -0700

    completed the first section of the homepage

--- mogged/src/components/home-section-1.jsx
+++ mogged/src/components/home-section-1.jsx
@@ -5,6 +5,21 @@ export default function Section1 () {
             <h3 className="home-subheadings acid spacer-2"> The Only Supplements Guaranteed To Unlock 
                 Your Inner Spartan
             </h3>
+            <p className="spacer-4 mogged-text white">In the first age, in the first time of strife, a lone Spartan warrior
+            wandered the land. He trained his body and his soul, fighting in battles for glory, <b className="acid">ONE MAN AGAINST AN ARMY. </b>
+            This man was fabled not only for his fighting ability and dank suit of Spartan armor, but also for his perfectly
+            symetrical and aesthetic physique. Said to glisten in the sun like a perfectly cut diamond, the Spartan's physique
+            became so impressive that he could conquer entire nations by simply <b className="acid">SPREADING HIS LATS. </b>The
+            Spartan soon became so jacked that he asceneded to god-hood and created a palace in the heavens, which he named <b className="acid">MOUNT SWOLYMPUS. </b>
+            To him, he gathered the swollest in all the lands and trained them into an elite guard, which he named the <b className="acid">THE CHAMPIONS OF SWOLYMPUS</b></p>
+
+            <p className="spacer-3 spacer-bottom-3 mogged-text white">Inspired by this story, Mogged harnesses the power of the Swole Spartan. 
+            Each of our products is forged from the very <b className="acid">SWEAT AND TEARS</b> of the Swole Spartan and the Champions of Swolympus, enabling our customers to
+            recieve a measure of their <b className="acid">HOLY STRENGTH.</b> Every champion, legend, and legacy comes from humble
+            beginnings. Mogged believes that with the right <b className="acid">MINDSET, TRAINING, AND SUPPLEMENTS, </b>you can forge a 
+            body worthy of Champion status on Mount Swolympus. So what are you waiting for? Glory doesn't come to 
+            those who dont train. Get up, hit the weights, grab some mogged, and start getting <b className="acid">YOUR SWOLLEST.</b></p>
+            <button className="home-button-1 spacer-bottom-3">Browse The Line</button>
         </div>
     )
 }
