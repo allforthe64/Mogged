4d58dd853a886110458fdb568408a3cea8e46dbd
William Rainey <williamhrainey@gmail.com>
2022-09-22 15:28:23 -0700

    added second section to the homepage

--- mogged/src/components/home-main.jsx
+++ mogged/src/components/home-main.jsx
@@ -1,9 +1,11 @@
 import Section1 from './home-section-1'
+import Section2 from './home-section-2'
 
 export default function HomeMain () {
     return (
         <div>
             <Section1 />
+            <Section2 />
         </div>
     )
 }
---
+++ mogged/src/components/home-section-2.jsx
@@ -0,0 +1,10 @@
+export default function Section2 () {
+    return (
+        <div className="sec-2-main">
+            <div className="sec-2-con spacer-4 spacer-bottom-4">
+                <h1 className="home-header-2 purple">Three Products. Three
+                Different Ways To Get Absolutely Massive</h1>
+            </div>
+        </div>
+    )
+}
