5e2097284071bce21fc94d94b95ac641584b7766
William Rainey <williamhrainey@gmail.com>
2022-09-26 09:13:12 -0700

    begun building out product card components

---
+++ mogged/src/components/ProductCard.jsx
@@ -0,0 +1,8 @@
+export default function ProductCard(props) {
+    return (
+        <div className='productCard spacer-3'>
+            <img src={props.img} alt="protein" className="card-img" />
+
+        </div>
+    )
+}
--- mogged/src/components/home-section-2.jsx
+++ mogged/src/components/home-section-2.jsx
@@ -1,10 +1,16 @@
+import ProductCard from './ProductCard'
+import Img from '../assets/protein.png'
+
 export default function Section2 () {
     return (
-        <div className="sec-2-main">
-            <div className="sec-2-con spacer-4 spacer-bottom-4">
-                <h1 className="home-header-2 purple">Three Products. Three
-                Different Ways To Get Absolutely Massive</h1>
+        <div className='white-background'>
+            <div className="sec-2-main">
+                <div className="sec-2-con spacer-4 spacer-bottom-3">
+                    <h1 className="home-header-2 purple">Three Products. Three
+                    Different Ways To Get Absolutely Massive</h1>
+                </div>
             </div>
+            <ProductCard img={Img}/>
         </div>
     )
 }
