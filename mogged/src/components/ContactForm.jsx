6450f19fcb1891da53e79bdee325e6ac30bfdc02
William Rainey <williamhrainey@gmail.com>
2022-10-12 13:01:32 -0700

    added the contact form

---
+++ mogged/src/components/ContactForm.jsx
@@ -0,0 +1,12 @@
+export default function ContactForm () {
+    return (
+        <div className="container spacer-bottom-4">
+            <div className="contact-form-container">
+                <h2 className="spacer-4 purple contact-header-2 spacer-bottom-4">Leave A Comment:</h2>
+                <form className="contact-form purple-background spacer-bottom-5">
+
+                </form>
+            </div>
+        </div>
+    )
+}
