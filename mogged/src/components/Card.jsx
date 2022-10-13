ee311dae95c72759e9325763df9c379632fa99cc
William Rainey <williamhrainey@gmail.com>
2022-10-13 13:54:12 -0700

    completed card components

--- mogged/src/components/Card.jsx
+++ mogged/src/components/Card.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Card (props) {
     return (
         <div>
-            <div className="card"></div>
+            <div className="card">
+                <img src={props.img} className='card-img spacer-bottom'></img>
+                <Link className="Link acid-background">{props.name}</Link>
+            </div>
         </div>
     )
 }
--- mogged/src/components/shop.jsx
+++ mogged/src/components/shop.jsx
@@ -4,24 +4,35 @@ import Card from './Card'
 export default function Shop() {
 
     const cards = data.map(product => {
-        return <Card name={product.product}/>
+        return <Card name={product.product} img={product.img}/>
     })
 
     console.log(cards)
 
     return (
-        <div className="container">
-            <div>
-                <h1 className="shop-header purple spacer-3">Shop The Line</h1>
-                <p className="purple shop-caption spacer-1">View Our Line Of Premium Products Below:</p>
-                <div className='container spacer-4'>
-                    <div className='card-container'>
-                        {cards[0]}
-                        {cards[1]}
-                        {cards[2]}
-                    </div>
+        <>
+            <div className="container">
+                <div>
+                    <h1 className="shop-header purple spacer-3">Shop The Line</h1>
+                    <p className="purple shop-caption spacer-1">View Our Line Of Premium Products Below:</p>
                 </div>
             </div>
-        </div>
+            <div className='container spacer-4'>
+                <p className='purple shop-header-2'>Supplements</p>
+            </div>
+            <div className='card-container spacer-3'>
+                    {cards[0]}
+                    {cards[1]}
+                    {cards[2]}
+            </div>
+            <div className='container spacer-6'>
+                <p className='purple shop-header-2'>Clothing</p>
+            </div>
+            <div className='card-container spacer-3 spacer-bottom-6'>
+                    {cards[3]}
+                    {cards[4]}
+                    {cards[5]}
+            </div>
+        </>
     )
 }
