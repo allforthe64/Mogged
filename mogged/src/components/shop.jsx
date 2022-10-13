c358344dd112f798de03a4cecd30807e462a5a8b
William Rainey <williamhrainey@gmail.com>
2022-10-12 17:45:40 -0700

    working on the cards

--- mogged/src/components/shop.jsx
+++ mogged/src/components/shop.jsx
@@ -1,9 +1,26 @@
+import data from '../data/product-info'
+import Card from './Card'
+
 export default function Shop() {
+
+    const cards = data.map(product => {
+        return <Card name={product.product}/>
+    })
+
+    console.log(cards)
+
     return (
         <div className="container">
             <div>
                 <h1 className="shop-header purple spacer-3">Shop The Line</h1>
                 <p className="purple shop-caption spacer-1">View Our Line Of Premium Products Below:</p>
+                <div className='container spacer-4'>
+                    <div className='card-container'>
+                        {cards[0]}
+                        {cards[1]}
+                        {cards[2]}
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ mogged/src/data/product-info.js
@@ -0,0 +1,48 @@
+import img1 from '../assets/protein.png'
+import img2 from '../assets/creatine.png'
+import img3 from '../assets/preworkout.png'
+
+export default [
+    {
+        id: 1,
+        product: 'Mogged Protein',
+        img: img1,
+        price: '$75.00',
+        description: 'test description [insert real description at later date]'
+    },
+    {
+        id: 2,
+        product: 'Cretaceous Creatine',
+        img: img2,
+        price: '$50.00',
+        description: 'test description [insert real description at later date]'
+    },
+    {
+        id: 3,
+        product: 'Power Punch Preworkout',
+        img: img3,
+        price: '$35.00',
+        description: 'test description [insert real description at later date]'
+    },
+    {
+        id: 4,
+        product: 'Mogged Swolympus T-Shirt',
+        img: '',
+        price: '$20.00',
+        description: 'test description [insert real description at later date]'
+    },
+    {
+        id: 5,
+        product: 'The Grim Lifter Oversized T',
+        img: '',
+        price: '$30.00',
+        description: 'test description [insert real description at later date]'
+    },
+    {
+        id: 6,
+        product: 'Swole Spartan Hoodie',
+        img: '',
+        price: '$75.00',
+        description: 'test description [insert real description at later date]'
+    }
+]
