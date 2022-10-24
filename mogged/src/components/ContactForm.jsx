1db4ef6627fc0011b2c4c92b3996e0f7505cec11
William Rainey <williamhrainey@gmail.com>
2022-10-24 14:34:13 -0700

    added useRef and clearing form

--- mogged/src/components/ContactForm.jsx
+++ mogged/src/components/ContactForm.jsx
@@ -1,21 +1,38 @@
+import { useRef } from 'react'
+
 export default function ContactForm () {
+
+    const emailEl = useRef('')
+    const fName = useRef('')
+    const lName = useRef('')
+    const comments = useRef('')
+
+    function clearForm () {
+        emailEl.current.value = ''
+        fName.current.value = ''
+        lName.current.value = ''
+        comments.current.value = ''
+
+        
+    }
+
     return (
         <div className="container spacer-bottom-4">
             <div className="contact-form-container">
                 <h2 className="spacer-4 purple contact-header-2 spacer-bottom-4">Leave A Comment:</h2>
-                <form className="contact-form purple-background spacer-bottom-5">
-                    <p className="acid form-headers spacer-3">Email</p>
-                    <input type='email' className="form-input"/>
+                <div className="contact-form purple-background spacer-bottom-5">
+                    <p className="acid form-headers spacer-3" >Email</p>
+                    <input type='email' className="form-input" ref={emailEl}/>
                     <p className='acid form-headers spacer-3'>First Name</p>
-                    <input type='text' className="form-input"/>
+                    <input type='text' className="form-input" ref={fName}/>
                     <p className='acid form-headers spacer-3'>Last Name</p>
-                    <input type='text' className="form-input"/>
+                    <input type='text' className="form-input" ref={lName}/>
                     <p className='acid form-headers spacer-3'>Comments</p>
-                    <textarea rows='10' cols='80' className="form-text spacer-bottom-3"></textarea>
+                    <textarea rows='10' cols='50' className="form-text spacer-bottom-3" ref={comments}></textarea>
                     <div className="form-button-con spacer-2">
-                        <button type="submit" className="spacer-bottom-3 form-button">Submit</button>
+                        <button className="spacer-bottom-3 form-button" onClick={() => clearForm()}>Submit</button>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     )
