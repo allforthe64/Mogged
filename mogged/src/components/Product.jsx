843e0d62e5b08704dc91e8f84cf762ceb01b0219
William Rainey <williamhrainey@gmail.com>
2022-10-27 21:37:35 -0700

    optomized product pages for mobile

--- mogged/src/components/Product.jsx
+++ mogged/src/components/Product.jsx
@@ -15,12 +15,13 @@ export default function Product (props) {
 
     return (
         <div className="product spacer-3 spacer-bottom-4">
+            <h1 className="purple product-header-alt">{obj.product}</h1>
             <img className="product-img" src={obj.img}></img>
             <div className="product-info">
                 <h1 className="purple product-header">{obj.product}</h1>
                 <p className="description spacer-2">{obj.description}</p>
                 <div className="order-con">
-                    <p className="price spacer-3">Price: {obj.price}</p>
+                    <p className="price-product spacer-3">Price: {obj.price}</p>
                     <button className="acid purple-background add-button" onClick={() => props.func(obj.product)}>Add To Cart</button>
                 </div>
             </div>
